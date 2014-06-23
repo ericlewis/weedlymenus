32e7902de88d02abb96638cc8656e9fb4118cf47
Eric Lewis <ericlewis777@gmail.com>
2014-06-22 22:48:16 -0700

    Bay Meds Delivery Menu Updated

--- diffscript.sh
+++ diffscript.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 curl 'https://api.legalmarijuanadispensary.com/dispensaries/28795/menu_items.json' | python -mjson.tool > "menus/28795.json"
 git add --all . && \
-git commit -am"Updated Bay Meds Delivery Menu" && \
+git commit -am"Bay Meds Delivery Menu Updated" && \
 git push origin master
